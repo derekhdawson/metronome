ad0eb59e066fe09ed7768423e5f718d932c58038
Derek Dawson <derekhdawson@yahoo.com>
2020-08-21 22:54:28 -0700

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import * as React from 'react';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { Home, Foo, Bar } from './Pages';
 
-type AppProps = { message: string }; /* could also use interface */
-const App: React.FunctionComponent<AppProps> = ({ message }: AppProps) => <div>{message}</div>;
-export default App;
+export default class App extends React.Component {
+    render(): JSX.Element {
+        return (
+            <Router>
+                <div>
+                    <nav>
+                        <Link to="/">Home</Link>
+                        <Link to="/foo">Foo</Link>
+                        <Link to="/bar">Bar</Link>
+                    </nav>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/foo" component={Foo} />
+                        <Route exact path="/bar" component={Bar} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
