b89429fb120cf6250b9a7cf2c7088916887e9b74
Derek Dawson <derekhdawson@yahoo.com>
2020-08-27 14:46:22 -0700

    add plus minus buttons to increment and decrement bpm

--- src/pages/MetronomePage/components/BPMSliderComponent.tsx
+++ src/pages/MetronomePage/components/BPMSliderComponent.tsx
@@ -4,15 +4,30 @@ import '../styles/bpm-component.scss';
 const MIN_BPM = 20;
 const MAX_BPM = 260;
 
+const INCREMENT_STR = 'increment';
+const DECREMENT_STR = 'decrement';
+
 interface Props {
     bpm: number;
     setBPM: (bpm: number) => void;
 }
 
 const BPMSliderComponent: FunctionComponent<Props> = (props: Props) => {
+    type IncrementDecrementButtonType = typeof INCREMENT_STR | typeof DECREMENT_STR;
+    const incrementDecrementBPMButton = (typeOfButton: IncrementDecrementButtonType): JSX.Element => {
+        const incrementAmount = typeOfButton === INCREMENT_STR ? 1 : -1;
+        const buttonText = typeOfButton === INCREMENT_STR ? '+' : '-';
+        return (
+            <div className="bpm-slider-increment-decrement-button-container">
+                <span className="bpm-slider-increment-decrement-button-text">{buttonText}</span>
+            </div>
+        );
+    };
+
     const { bpm } = props;
     return (
         <div className="bpm-slider-container">
+            {incrementDecrementBPMButton(INCREMENT_STR)}
             <input
                 className="bpm-slider"
                 id="bpm-slider"
@@ -26,6 +41,7 @@ const BPMSliderComponent: FunctionComponent<Props> = (props: Props) => {
                     props.setBPM(parseInt(event.target.value, 10));
                 }}
             ></input>
+            {incrementDecrementBPMButton(DECREMENT_STR)}
         </div>
     );
 };
